4c3f60ec9dcf2ecfa3c0b7e4a5baffae197fd788
LyanRodrigues <galanislyan@gmail.com>
2024-08-13 21:43:26 -0300

    upd

--- Aula/Questao1/ListaEstatica.java
+++ Aula/Questao1/ListaEstatica.java
@@ -1,72 +1,70 @@
 package Aula.Questao1;
 
-public class ListaEstatica {
-    private Object[] info;
+public class ListaEstatica<T> {
+    private T[] info;
     private int tamanho;
 
+    @SuppressWarnings("unchecked")
     public ListaEstatica() {
-        this.info = new Object[10];
+        this.info = (T[]) new Object[10]; // Use of unchecked cast
         this.tamanho = 0;
     }
 
-    private void redimensionar(){
-        Object[] novoVetor = new Object[info.length + 10];
-        for(int i = 0; i < info.length; i++){
-            novoVetor[i] = info[i];
-        }
+    private void redimensionar() {
+        T[] novoVetor = (T[]) new Object[info.length + 10]; // Use generic type
+        System.arraycopy(info, 0, novoVetor, 0, info.length);
         info = novoVetor;
     }
 
-    public void inserir(Object valor){
-        if(tamanho == info.length){
+    public void inserir(T valor) {
+        if (tamanho == info.length) {
             redimensionar();
         }
         info[tamanho] = valor;
         tamanho++;
     }
 
-    public void exibir(){
-        for (int i = 0; i < tamanho; i++){
+    public void exibir() {
+        for (int i = 0; i < tamanho; i++) {
             System.out.println(info[i]);
         }
     }
 
-    public int buscar(Object valor){
-        for (int i = 0; i < tamanho; i++){
-            if (info[i].equals(valor)){
+    public int buscar(T valor) {
+        for (int i = 0; i < tamanho; i++) {
+            if (info[i].equals(valor)) {
                 return i;
             }
         }
         return -1;
     }
-    
 
-    public void retirar(Object valor){
+    public void retirar(T valor) {
         for (int i = 0; i < tamanho; i++) {
             if (info[i].equals(valor)) {
                 for (int x = i; x < tamanho - 1; x++) {
                     info[x] = info[x + 1];
                 }
+                info[tamanho - 1] = null; // Avoid memory leak
                 tamanho--;
-                break; // Exit after removing the element
+                break;
             }
         }
     }
-    
 
-    public void liberar(){
-        info = new Object[10];
+    public void liberar() {
+        info = (T[]) new Object[10];
         tamanho = 0;
     }
 
-    public Object obterElemento(int posicao){
-        if(posicao >= 0 && posicao < tamanho){
+    public T obterElemento(int posicao) {
+        if (posicao >= 0 && posicao < tamanho) {
             return info[posicao];
         }
         throw new IndexOutOfBoundsException("Posição inválida!");
     }
 
-    public boolean estaVazia(){
+    public boolean estaVazia() {
         return tamanho == 0;
     }
 
@@ -74,15 +72,15 @@ public class ListaEstatica {
         return tamanho;
     }
 
-    public String toString(){
+    @Override
+    public String toString() {
         StringBuilder res = new StringBuilder();
-        for (int i = 0; i < tamanho; i++){
-            if(i > 0){
+        for (int i = 0; i < tamanho; i++) {
+            if (i > 0) {
                 res.append(", ");
             }
             res.append(info[i].toString());
         }
         return res.toString();
     }
-    
 }
